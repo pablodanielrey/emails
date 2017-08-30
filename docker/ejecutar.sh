fb4340478b68239c9f52f9ad94498b1a241498da
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-30 17:58:42 -0300

    agregue el ejecutar para que cumpla con los otros sistemas

---
+++ docker/ejecutar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo docker run -ti --rm -d --name emails -v $(pwd)/src:/src -p 5025:5000 -p 5026:5001 -p 5027:5002 -p 5028:5003 --env-file $HOME/gitlab/fce/produccion/emails emails
+sudo docker exec -t emails bash instalar.sh
+
