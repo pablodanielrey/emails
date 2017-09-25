74b0039b0511861542fb9bb293ce0c2f4bd670db
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-09-25 14:22:10 -0300

    implemente el microservices_common para obtener codigo comun

--- docker/ejecutar-pablo-casa.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -ti --name emails -v $(pwd)/src:/src -p 8000:5000 -p 8001:5001 -p 8002:5002 --env-file /home/pablo/gitlab/fce/pablo/casa/emails emails
--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker run -ti -d --name emails -v $(pwd)/src:/src -p 5025:5000 -p 5026:5001 -p 5027:5002 -p 5028:5003 --env-file $HOME/gitlab/fce/produccion/emails emails
 sudo docker exec -t emails bash instalar.sh
-
+sudo docker restart emails
