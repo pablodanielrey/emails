32e283ce233a19543c3cc6eccd99e4accf9d2444
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-10-11 15:18:05 -0300

    corregi el nombre del script

--- docker/src/registrar.sh
+++ docker/src/registrar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 emails/registrar.py emails_web / emails.econo.unlp.edu.ar:5025 &
-python3 emails/registrar.py emails_rest /emails/api emails.econo.unlp.edu.ar:5026
+python3 emails/register.py emails_web / emails.econo.unlp.edu.ar:5025 &
+python3 emails/register.py emails_rest /emails/api emails.econo.unlp.edu.ar:5026
